e699c911789df7e936fc72d7458ea2c2a2705da8
Manas Verma <manas03verma@gmail.com>
2026-07-10 15:36:26 +0530

    done with employee and attendance section

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin size-8 border-2 rounded-full h-12 w-12 border-t-transparent border-blue-500"/>
+    </div>
+  )
+}
+
+export default Loading
